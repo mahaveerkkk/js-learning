4432c294431dabc5624052bbbb6507c0300f7828
Mahaveer <k725182@gmail.com>
2026-07-20 22:26:53 +0530

    if else try

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
-console.log("Hello, World!");
-console.log(2);
-console.log(typeof 2);
-console.log(true);
-console.log(undefined);
-console.log(null);
-console.log(typeof null)
-console.log({});
-console.log([]);
-console.log(function() {});
-console.log(Array.isArray([]));
-console.log(Array.isArray({}));
+console.log(Boolean(0));
+console.log(Boolean(1));
+console.log(Boolean(""));
+console.log(Boolean(" "));
+console.log(Boolean("Hello"));
+console.log(Boolean(null));
+console.log(Boolean(undefined));
+console.log(Boolean([]));
+console.log(Boolean({}));
