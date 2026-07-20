ba101cf28ec7e3a8ba7e9702db146a979d879d1a
Mahaveer <k725182@gmail.com>
2026-07-20 22:49:26 +0530

    or ,and ,consepet

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ let guest = "Guest";
 
 console.log(user || guest);
 console.log("" || "Mahaveer");
+console.log(false || 0 || "" || null || undefined || "Mahaveer");
+
+console.log(1 && "Hello" && [] && {} && "JS");
+
+console.log("" && 100);
+
+console.log([] || {});
