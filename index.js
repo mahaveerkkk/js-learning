9166c16a8cb02825e1acaae89685ddd8dd9b6bbc
Mahaveer <k725182@gmail.com>
2026-07-22 22:06:48 +0530

    function are first class citizens

--- index.js
+++ index.js
@@ -1,53 +1,13 @@
-function showResult(name, marks){
-    if (name === "" || marks === "") {
-        return "Please enter both name and marks.";
-    }
-    if (marks < 0 || marks > 100) {
-        return "Marks should be between 0 and 100.";
-    }
-    if (marks >= 90) {
-        grade ="A";
-    } else if (marks >= 80) {
-        grade = "B";
-    } else if (marks >= 70) {
-        grade = "C";
-    } else if (marks >= 60) {
-        grade = "D";
-    } else if (marks >= 50) {
-        grade = "E";
-    }else {
-        grade = "F";
-    }
-    if (grade==="F") {
-        status = "Fail";
-    } else {
-        status = "Pass";
-    }
-     
-    // return name + " -> " + grade + " -> " + status;
-    console.log(`
-=========================
-Student Report
-=========================
-Name   : ${name}
-Marks  : ${marks}
-Grade  : ${grade}
-Result : ${status}
-=========================
-`);
+function add(a, b) {
+    return a + b;
 }
 
+function multiply(a, b) {
+    return a * b;
+}
 
-// +
-// console.log("=====================================");console.log("\n");
-// console.log("Student Report");console.log("\n");
-// console.log("=====================================");console.log("\n");
+function calculate(x, y, operation) {
+    return operation(x, y);
+}
 
-// console.log("Name:",name);
-// console.log("Marks:",marks);
-// console.log("Grade:",result);
-// console.log("Status:",status);
-
-
-//  console.log(name, "->",grade, "->", status);
-console.log(showResult("John Doe", 85));
+console.log(calculate(10, 20, multiply));
