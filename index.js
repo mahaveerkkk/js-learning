b459cc73715a58081a511736d557b521cbbae9b7
Mahaveer <k725182@gmail.com>
2026-07-25 21:21:09 +0530

    enen odd one line

--- index.js
+++ index.js
@@ -1,18 +1,2 @@
-//day 7 aairo fuction
-const hello = (name) =>{
-    return "hello " + name;
 
-}
-const squr =(num)=>{
-    return num*num;
-}
-const factorial = (num) =>{
-    if(num === 0 || num === 1){
-        return 1;
-    }else{
-        return num * factorial(num - 1);
-    }
-}
-console.log(hello("aairo"));
-console.log(squr(5));
-console.log(factorial(65));
+const evenOdd = (num) => (num % 2 === 0) ? "even" : "odd";
