e131b462dd9b126c4b7ed6d7c2bcecc65e044b0d
Mahaveer <k725182@gmail.com>
2026-07-27 22:11:23 +0530

    mini cart system

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 // day 10 Array  card system 
 
+
 const addItem = (item) => {
       cart.push(item)
       console.log(cart)
         
 }
 const removeLastItem = ()=>{
-    cart.pop
+    cart.pop()  
     console.log(cart)
 
 }
 const showCart = ()=>{
-    return console.log(cart)
+    
+    console.log("=====================")
+    console.log("Shoping Cart")
+    console.log("=====================")
+    for(let x=0; x<cart.length;x++){
+        console.log(x+1,".",cart[x])
+
+    }
+    console.log("Total Items :",cart.length)
 }
 
 
@@ -20,3 +29,4 @@ const cart = [];
 addItem("laptop")
 addItem("Mouse")
 addItem("Keywoard")
+showCart()
