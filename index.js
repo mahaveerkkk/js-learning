55202e0200106b9dfd3f62ba8d9ddc7f95d28477
Mahaveer <k725182@gmail.com>
2026-08-06 21:11:08 +0530

    basic forEach ,map ,find ,filter

--- index.js
+++ index.js
@@ -1,32 +1,62 @@
 // day 10 Array  card system 
 
 
-const addItem = (item) => {
-      cart.push(item)
-      console.log(cart)
+// const addItem = (item) => {
+//       cart.push(item)
+//       console.log(cart)
         
-}
-const removeLastItem = ()=>{
-    cart.pop()  
-    console.log(cart)
+// }
+// const removeLastItem = ()=>{
+//     cart.pop()  
+//     console.log(cart)
 
-}
-const showCart = ()=>{
+// }
+// const showCart = ()=>{
     
-    console.log("=====================")
-    console.log("Shoping Cart")
-    console.log("=====================")
-    for(let x=0; x<cart.length;x++){
-        console.log(x+1,".",cart[x])
+//     console.log("=====================")
+//     console.log("Shoping Cart")
+//     console.log("=====================")
+//     for(let x=0; x<cart.length;x++){
+//         console.log(x+1,".",cart[x])
 
+//     }
+//     console.log("Total Items :",cart.length)
+// }
+
+
+
+// const cart = [];
+// addItem("laptop")
+// addItem("Mouse")
+// addItem("Keywoard")
+// showCart()
+
+// we are learn new feature list new fuction like forEach , find,map ,filter
+
+let arr = ["rahul","madhu",54,65,94,{name:"mahaveer",agr:21},{name:"payal", age:20}]
+let data =arr.forEach((Val)=>{
+    console.log(Val)
+})
+//map
+let newarr= arr.map((dat)=>{
+    if (dat %2==0){
+        return dat**2
     }
-    console.log("Total Items :",cart.length)
-}
 
+})
+console.log(newarr)
 
+//filter
+let fil = arr.filter((v)=>{
+    if (v.name=="mahaveer"){
+        return v
+    }
 
-const cart = [];
-addItem("laptop")
-addItem("Mouse")
-addItem("Keywoard")
-showCart()
+})
+console.log(fil)
+console.log()
+// const fin= arr.find((a)=>{
+//     a.age===20
+
+// })
+// console.log(fin)
